9ba3d0bf2333333ae87c4bb75baf90832565cd4a
carlos-paezf <cpaezferreira@gmail.com>
2023-02-19 15:17:00 -0500

    Add Button Ripple Effect

---
+++ 20-Button_Ripple_Effect/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
