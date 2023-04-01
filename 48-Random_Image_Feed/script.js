11541f09ab56a51bcfb73300efb68051d6701b26
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-04-01 17:55:47 -0500

    Add Random Image Feed

---
+++ 48-Random_Image_Feed/script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+const unsplashURL = 'https://source.unsplash.com/random/';
+const rows = 5;
+
+
+for (let i = 0; i < rows * 3; i++) {
+    const img = document.createElement('img');
+    img.src = `${ unsplashURL }${ getRandomSize() }`;
+    container.appendChild(img);
+}
+
+
+function getRandomSize () {
+    return `${ getRandomNr() }${ getRandomNr() }`;
+}
+
+
+function getRandomNr () {
+    return Math.floor(Math.random() * 10) + 300;
+}
